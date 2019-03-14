603bcbd619438a23f656dd197fa5b9b546741d24
Balint <korosibalint@yahoo.co.uk>
2019-03-14 14:32:03 +0100

    change time to pressure

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,8 +2,8 @@ const endpoint =
   "https://mars.nasa.gov/rss/api/?feed=weather&category=insight&feedtype=json&ver=1.0";
 const uiMinTemp = document.querySelector("#min_temp");
 const uiMaxTemp = document.querySelector("#max_temp");
-const uiSunrise = document.querySelector("#sunrise");
-const uiSunset = document.querySelector("#sunset");
+const uiMinPressure = document.querySelector("#sunrise");
+const uiMaxPressure = document.querySelector("#sunset");
 
 let data;
 let latestSol;
@@ -17,9 +17,17 @@ fetch(endpoint)
     sol_keys = raw.sol_keys;
     latestSol = sol_keys[sol_keys.length - 1];
 
+    // Min and Max temperature from API
     uiMinTemp.textContent = Math.round(data[latestSol].AT.mn);
     uiMaxTemp.textContent = Math.round(data[latestSol].AT.mx);
 
+    uiMinPressure.textContent = Math.round(data[latestSol].PRE.mn) + " Pa";
+    uiMaxPressure.textContent = Math.round(data[latestSol].PRE.mx) + " Pa";
+
+    // Pressure
+
+    // Not sure what these times are in data
+    /* 
     sunsetDate = new Date(data[latestSol].First_UTC);
     sunriseDate = new Date(data[latestSol].Last_UTC);
     uiSunrise.textContent =
@@ -27,21 +35,5 @@ fetch(endpoint)
 
     uiSunset.textContent =
       sunsetDate.getHours() + ":" + sunriseDate.getMinutes();
+      */
   });
-
-/*
-console.log(data);
-TZ_Data: "America/Port_of_Spain"
-atmo_opacity: "Sunny"
-id: 71
-max_gts_temp: 5
-max_temp: -17
-min_gts_temp: -74
-min_temp: -72
-season: "Month 12"
-sol: 2319
-status: 200
-sunrise: "06:46"
-sunset: "18:52"
-unitOfMeasure: "Celsius"
-*/
